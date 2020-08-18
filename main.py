412894ec861545436c6dc707e62045d44b08ff4d
luca-greenshields <65540293+luca-greenshields@users.noreply.github.com>
2020-08-18 18:42:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,36 +5,40 @@ def user_input():
     return(num_list)   
 
 
-def average(nlist):
-    n = len(nlist)
-    sum_n = sum(nlist)
+def average(n_list):
+    n = len(n_list)
+    sum_n = sum(n_list)
     average = sum_n / n
     return(average)
-print(f"average is {average(num_list)}")
+
 
 
 def median(n_list):
-    n = len(num_list)
-    n_sort = num_list.sort()
+    n = len(n_list)
+    n_sort = n_list.sort()
 
     if n % 2 == 0:
-        median1 = num_list[n//2]
-        median2 = num_list[n//2-1]
+        median1 = n_list[n//2]
+        median2 = n_list[n//2-1]
         median = (median1 + median2)/2
     else:
-        median = num_list[n//2]    
+        median = n_list[n//2]    
     return(median)
 
-#num_list = [1, 4, 2, 5, 3]
+
+def mode(n_list):
+    freq_dict = {}
+    for num in n_list:
+        freq_dict[num] = freq_dict.setdefault(num, 0) + 1
+    freq_list = [ (key, value) for key, value in freq_dict.items()]
+    sorted_freq_list = sorted(freq_list, key=lambda item: item[1], reverse=True) 
+    max_freq = sorted_freq_list[0][1]
+    result = [ item[0] for item in sorted_freq_list if item[1] == max_freq]
+    return(result)
+
+num_list = user_input()
+print(f"average is {average(num_list)}")
 print(f"median is {median(num_list)}")
-
-
-
-
-
-
-
-#num_list = user_input()
-#print(f"average is {average(num_list)}")
+print(f"mode is {mode(num_list)}")
 
 
